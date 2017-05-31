4225f647750b35f74064376d778bd6573806a46a
Florian Simon <florian@tentwentyfour.lu>
2017-05-31 09:27:35 +0200

    Remove reference to removed variable body

--- dist/ionic.content.banner.js
+++ dist/ionic.content.banner.js
@@ -58,22 +58,7 @@ angular.module('jett.ionic.content.banner', ['ionic']);
       '$log',
       function ($document, $rootScope, $compile, $timeout, $ionicPlatform, $log) {
         var cacheIndex = 0,
-          bannerCache = {};
-
-        function isActiveView (node) {
-          // walk up the child-parent node chain until we get to the root or the BODY
-          while (node !== null && node.nodeName !== 'BODY') {
-            var navView = node.getAttribute("nav-view");
-
-            // as soon as we encounter a cached (parent) view then we know the view can't be active
-            if (navView !== null && navView === 'cached') {
-              return false;
-            }
-            node = node.parentNode;
-          }
-          // no cached parent seen, the view must be really active
-          return true;
-        }
+            bannerCache = {};
 
         function cacheBanner(scope){
           bannerCache[cacheIndex] = scope;
@@ -149,7 +134,7 @@ angular.module('jett.ionic.content.banner', ['ionic']);
               $timeout(function () {
                 scope.$destroy();
                 element.remove();
-                body = stateChangeListenDone = null;
+                stateChangeListenDone = null;
               }, 400);
             });
 
--- js/ionic.content.banner.service.js
+++ js/ionic.content.banner.service.js
@@ -18,22 +18,7 @@
       '$log',
       function ($document, $rootScope, $compile, $timeout, $ionicPlatform, $log) {
         var cacheIndex = 0,
-          bannerCache = {};
-
-        function isActiveView (node) {
-          // walk up the child-parent node chain until we get to the root or the BODY
-          while (node !== null && node.nodeName !== 'BODY') {
-            var navView = node.getAttribute("nav-view");
-
-            // as soon as we encounter a cached (parent) view then we know the view can't be active
-            if (navView !== null && navView === 'cached') {
-              return false;
-            }
-            node = node.parentNode;
-          }
-          // no cached parent seen, the view must be really active
-          return true;
-        }
+            bannerCache = {};
 
         function cacheBanner(scope){
           bannerCache[cacheIndex] = scope;
@@ -109,7 +94,7 @@
               $timeout(function () {
                 scope.$destroy();
                 element.remove();
-                body = stateChangeListenDone = null;
+                stateChangeListenDone = null;
               }, 400);
             });
 
